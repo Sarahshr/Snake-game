a1be1f47f54bf5e531ab23bc9e86a31f3eb4f054
Sarahshr <51380592+Sarahshr@users.noreply.github.com>
2021-01-16 23:48:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./components/home/Home";
 import Game from "./components/game/Game";
-//import Music from "./components/Music";
+
 const App = () => {
   const [alive, setAlive] = useState("start");
   const [score, setScore] = useState(0);
@@ -30,8 +30,3 @@ const App = () => {
 };
 
 export default App;
-//https://www.vectorstock.com/royalty-free-vector/set-seamless-patterns-green-and-dry-grass-in-vector-30385863
-//https://www.vectorstock.com/royalty-free-vector/set-seamless-texture-ground-with-small-stones-vector-25825290
-//https://fontmeme.com/polices/police-just-kids/
-//https://www.storyblocks.com/audio/search/game?media-type=music&search-origin=filters&sort=most_relevant&vbonly=false&categories=kids-and-family
-//http://www.jeux.org/jeu/snake-game.html
